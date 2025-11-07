c5adf915303f1387fac0aef7c02042ac3b16663f
DoFlow-spec <majdeltacrypto@gmail.com>
2025-11-08 00:49:56 +0530

    chnage cookie options

--- Backend/src/const.js
+++ Backend/src/const.js
@@ -5,17 +5,26 @@ const parseEnvTime = timeStr => {
   if (timeStr.endsWith('m')) return value * 60 * 1000;
   return value * 1000;
 };
+
+const DEFAULT_ACCESS_EXPIRY = 15 * 60 * 1000;         // 15 minutes
+const DEFAULT_REFRESH_EXPIRY = 15 * 24 * 60 * 60 * 1000;
+
 export const getCookieOptions = (type = 'access') => {
-  const expiry =
+ const expiryEnv =
     type === 'access'
       ? process.env.ACCESS_TOKEN_EXPIRY
       : process.env.REFRESH_TOKEN_EXPIRY;
 
+  const maxAge =
+    parseEnvTime(expiryEnv) ||
+    (type === 'access' ? DEFAULT_ACCESS_EXPIRY : DEFAULT_REFRESH_EXPIRY);
+
+
   return {
-    httpOnly: true,
-    secure: process.env.NODE_ENV === 'production',
-    sameSite: 'lax',
-    path: '/',
-    maxAge: parseEnvTime(expiry),
+    httpOnly: true,                      
+    secure: true,                
+    sameSite: 'none' ,
+    path: '/',                            
+    maxAge,
   };
 };
