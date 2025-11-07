100e886754fc5a788ddb912c05b8e31cb6560160
DoFlow-spec <majdeltacrypto@gmail.com>
2025-11-08 01:05:40 +0530

    smtp

--- Backend/src/service/SendEmail.js
+++ Backend/src/service/SendEmail.js
@@ -1,9 +1,14 @@
 import nodemailer from 'nodemailer';
+import dotenv from "dotenv"
+
+dotenv.config()
 
 export const sendEmail = async ({ to, subject, html }) => {
   try {
     const transporter = nodemailer.createTransport({
-      service: 'gmail',
+      host: 'smtp.gmail.com', 
+      port: 465,            
+      secure: true,   
       auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS,
